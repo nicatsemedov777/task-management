f934a3a50a2c4790aee6cdfa86e5e4de9cbc2b4c
nicatsemedov777 <smdovn42@gmail.com>
2023-12-02 14:56:19 +0400

    init

--- src/main/java/az/iktlab/taskmanagement/controller/TaskController.java
+++ src/main/java/az/iktlab/taskmanagement/controller/TaskController.java
@@ -4,6 +4,8 @@ import az.iktlab.taskmanagement.model.request.TaskCreateRequest;
 import az.iktlab.taskmanagement.model.request.TaskUpdateRequest;
 import az.iktlab.taskmanagement.model.response.TaskResponse;
 import az.iktlab.taskmanagement.service.TaskService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -17,11 +19,35 @@ public class TaskController {
     private final TaskService taskService;
 
     @PostMapping
+    @Operation(summary = "This endpoint help us to add new tasks",
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "The request was successful"),
+                    @ApiResponse(responseCode = "400",
+                            description = "There is incoming request validation error"),
+                    @ApiResponse(responseCode = "409",
+                            description = "There is a conflict with the current state of the resource, " +
+                                    "preventing the request from being completed."),
+                    @ApiResponse(responseCode = "417",
+                            description = "The server cannot meet the expectations specified in the request"),
+                    @ApiResponse(responseCode = "500", description = "An unexpected error occurred on the server.")
+            })
     public TaskResponse create(@RequestBody TaskCreateRequest taskCreateRequest) {
         return taskService.create(taskCreateRequest);
     }
 
     @DeleteMapping
+    @Operation(summary = "This endpoint help us to delete tasks by category name ",
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "The request was successful"),
+                    @ApiResponse(responseCode = "400",
+                            description = "There is incoming request validation error"),
+                    @ApiResponse(responseCode = "409",
+                            description = "There is a conflict with the current state of the resource, " +
+                                    "preventing the request from being completed."),
+                    @ApiResponse(responseCode = "417",
+                            description = "The server cannot meet the expectations specified in the request"),
+                    @ApiResponse(responseCode = "500", description = "An unexpected error occurred on the server.")
+            })
     public HttpStatus deleteAllByCategory(@RequestParam String categoryName) {
         taskService.deleteAllByCategory(categoryName);
         return HttpStatus.OK;
@@ -44,6 +70,18 @@ public class TaskController {
     }
 
     @PutMapping("{id}")
+    @Operation(summary = "This endpoint help us to update task",
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "The request was successful"),
+                    @ApiResponse(responseCode = "400",
+                            description = "There is incoming request validation error"),
+                    @ApiResponse(responseCode = "409",
+                            description = "There is a conflict with the current state of the resource, " +
+                                    "preventing the request from being completed."),
+                    @ApiResponse(responseCode = "417",
+                            description = "The server cannot meet the expectations specified in the request"),
+                    @ApiResponse(responseCode = "500", description = "An unexpected error occurred on the server.")
+            })
     public TaskResponse update(@PathVariable String id,@RequestBody TaskUpdateRequest taskUpdateRequest) {
         return taskService.update(id,taskUpdateRequest);
     }
--- src/main/java/az/iktlab/taskmanagement/controller/UserController.java
+++ src/main/java/az/iktlab/taskmanagement/controller/UserController.java
@@ -6,6 +6,8 @@ import az.iktlab.taskmanagement.model.request.UserRecoverAccountOTPRequest;
 import az.iktlab.taskmanagement.model.request.UserRecoverAccountRequest;
 import az.iktlab.taskmanagement.model.request.UserSignInRequest;
 import az.iktlab.taskmanagement.service.UserService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
@@ -17,15 +19,51 @@ public class UserController {
     private final UserService userService;
 
     @PostMapping("/sign-up")
+    @Operation(summary = "This endpoint help us to register",
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "The request was successful"),
+                    @ApiResponse(responseCode = "400",
+                            description = "There is incoming request validation error"),
+                    @ApiResponse(responseCode = "409",
+                            description = "There is a conflict with the current state of the resource, " +
+                                    "preventing the request from being completed."),
+                    @ApiResponse(responseCode = "417",
+                            description = "The server cannot meet the expectations specified in the request"),
+                    @ApiResponse(responseCode = "500", description = "An unexpected error occurred on the server.")
+            })
     public JWTToken signUp(@RequestBody @Valid UserCreateRequest userCreateRequest) {
        return userService.signUp(userCreateRequest);
     }
     @PostMapping("/sign-in")
+    @Operation(summary = "This endpoint help us to login",
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "The request was successful"),
+                    @ApiResponse(responseCode = "400",
+                            description = "There is incoming request validation error"),
+                    @ApiResponse(responseCode = "409",
+                            description = "There is a conflict with the current state of the resource, " +
+                                    "preventing the request from being completed."),
+                    @ApiResponse(responseCode = "417",
+                            description = "The server cannot meet the expectations specified in the request"),
+                    @ApiResponse(responseCode = "500", description = "An unexpected error occurred on the server.")
+            })
     public JWTToken signIn(@RequestBody @Valid UserSignInRequest userSignInRequest) {
         return userService.signIn(userSignInRequest);
     }
 
     @PostMapping("/recover")
+    @Operation(summary = "This endpoint help us to recover",
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "The request was successful"),
+                    @ApiResponse(responseCode = "400",
+                            description = "There is incoming request validation error"),
+                    @ApiResponse(responseCode = "409",
+                            description = "There is a conflict with the current state of the resource, " +
+                                    "preventing the request from being completed."),
+                    @ApiResponse(responseCode = "417",
+                            description = "The server cannot meet the expectations specified in the request"),
+                    @ApiResponse(responseCode = "500", description = "An unexpected error occurred on the server.")
+            })
     public void sendOTP(@RequestBody @Valid UserRecoverAccountRequest userRecoverAccountRequest) {
         userService.sendOTP(userRecoverAccountRequest);
     }
